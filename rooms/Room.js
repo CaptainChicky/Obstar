0fde1354e8f2d04a1b41f11eb1f81064dceb857b
CaptainChicky <70708832+CaptainChicky@users.noreply.github.com>
2026-07-23 21:00:54 -0400

    m

--- rooms/Room.js
+++ rooms/Room.js
@@ -39,13 +39,10 @@
 	(lib/runtime.js) because the dependency graph is circular - see the note there.
 */
 const RT = require('../lib/runtime.js');
-const Vec = require('victor');
 const config = require('../lib/config.js').config;
 const termColors = require('../lib/terminal.js');
 const quadTree = require('../lib/quadTree.js');
 const CLASS = require('../public/SHARE/TanksConfig.js').class;
-const CLASS_TREE = require('../public/SHARE/TanksConfig.js').tree;
-const FRICTION = require('../lib/constants.js').FRICTION;
 const KIND = require('../public/SHARE/kinds.js');
 const clock = require('../lib/clock.js');
 
